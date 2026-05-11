ef9f1f89c224c24c0819b5fd57ecd5fdf813c967
Narasimharao Bhavirisetty <narasimharao2743@gmail.com>
2026-05-11 05:49:05 +0000

    Fix import: use langchain_text_splitters for newer langchain version

--- rag_pipeline.py
+++ rag_pipeline.py
@@ -1,6 +1,6 @@
 import os
 from langchain_community.document_loaders import PyPDFLoader
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_community.llms import Ollama
